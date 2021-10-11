4b69c416fe73c0f9d0908aad598bb59c6bc5628c
Rohan Mondal <mondalrohan108@gmail.com>
2021-10-11 23:59:32 +0530

    Update area.js

--- area.js
+++ area.js
@@ -6,6 +6,7 @@ const outputEl=document.querySelector("#output");
 function calculateArea(){
     const area= (parseInt(base.value)*parseInt(height.value))/2;
     if(Number(base.value)<0||Number(height.value)<0) outputEl.innerText="Base or Height can't be negative.";
+    else if(Number(base.value)===0|| Number(height.value)===0) outputEl.innerText="Pls enter the input correctly";
     else outputEl.innerText="The area is :"+area;
 }
 
